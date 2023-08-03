aa310c778d2f63c386228ae9ef0cad302852b16e
Valentim Helio de Oliveira <timvale@hotmail.com>
2023-08-03 08:58:56 -0400

    S6E33

--- eCommerce.API/Repositories/UsuarioRepository.cs
+++ eCommerce.API/Repositories/UsuarioRepository.cs
@@ -69,6 +69,8 @@ namespace eCommerce.API.Repositories
                 command.CommandText = "SELECT * FROM Usuarios u " +
                             " LEFT JOIN Contatos c  ON c.UsuarioId = u.Id " +
                             " LEFT JOIN EnderecosEntrega ee ON ee.UsuarioId = u.Id " +
+                            " LEFT JOIN UsuariosDepartamentos ud ON ud.UsuarioId = u.Id " +
+                            " LEFT JOIN Departamentos d ON d.Id = ud.DepartamentoId " +
                             " WHERE u.Id = @Id";
                 command.Parameters.AddWithValue("@Id", id);
                 command.Connection = (SqlConnection)_connection;
@@ -103,7 +105,8 @@ namespace eCommerce.API.Repositories
                         usuario.Contato = contato;
 
                         usuarios.Add(usuario.Id, usuario);
-                    } else
+                    }
+                    else
                     {
                         usuario = usuarios[dataReader.GetInt32(0)];
                     }
@@ -122,11 +125,28 @@ namespace eCommerce.API.Repositories
                     enderecoEntrega.Complemento = dataReader.GetString("Complemento");
 
                     usuario.EnderecosEntrega = (usuario.EnderecosEntrega == null) ? new List<EnderecoEntrega>() : usuario.EnderecosEntrega;
-                    usuario.EnderecosEntrega.Add(enderecoEntrega);
+                    if (usuario.EnderecosEntrega.FirstOrDefault(a => a.Id == enderecoEntrega.Id) == null)
+                    {
+                        usuario.EnderecosEntrega.Add(enderecoEntrega);
+                    }
 
+                    Departamento departamento = new Departamento();
+                    departamento.Id = dataReader.GetInt32(26);
+                    departamento.Nome = dataReader.GetString(27);
+
+                    usuario.Departamentos = (usuario.Departamentos == null) ? new List<Departamento>() : usuario.Departamentos;
+                    if (usuario.Departamentos.FirstOrDefault(a => a.Id == departamento.Id) == null)
+                    {
+                        usuario.Departamentos.Add(departamento);
+                    }
 
                 }
                 return usuarios[usuarios.Keys.First()];
+
+            }
+            catch
+            {
+                return null;
 
             }
             finally
